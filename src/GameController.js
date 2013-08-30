3a4b06268d5c1efa1960ec8464398e9f2379ee4d
jac1013 <jac.wshmstr@gmail.com>
2013-08-30 17:10:09 -0430

    added test for view

---
+++ src/GameController.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Joseph
+ * Date: 29/08/13
+ * Time: 08:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
---
+++ test/BowlingGameControllerTest.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Joseph
+ * Date: 30/08/13
+ * Time: 04:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
